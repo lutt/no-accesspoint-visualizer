f7e6edb5539254f45ba84f7a572c2a470800c2c9
lutt <lutt@anonymous.com>
2010-10-30 18:00:36 +0200

    add support for showing the APs on a map

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -15,8 +15,31 @@ var lib = {
 };
 
 $(function () {
-	var $body = $('body'),
+	var $body = $('#dynamic-content'),
 		$loader = null;
+		$mapContainer = $('#mapcontainer'),
+		mapController = null,
+		isMobileMode = !$mapContainer.is(':visible');
+
+	function getMap() {
+		if (mapController !== null) {
+			return mapController;
+		}
+
+		var mapCanvas = $('<div></div>');
+		mapCanvas.attr('id', 'mapcanvas');
+		mapCanvas.css({
+			width: '100%',
+			height: '100%'
+		});
+		$mapContainer.html(mapCanvas);
+
+		mapController = new google.maps.Map(document.getElementById("mapcanvas"), {
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		});
+
+		return mapController;
+	}
 
 	function foundPosition(position) {
 		var pos = [ position.coords.latitude,
@@ -30,7 +53,14 @@ $(function () {
 				return;
 			}
 
-			var $list = $('<ul id="list"></ul>'), i;
+			var $list = $('<ul id="list"></ul>'),
+				map = getMap(),
+				i,
+				centerCoords = null;
+
+			if (isMobileMode === false) {
+				centerCoords = new google.maps.LatLngBounds();
+			}
 
 			// add distance to the array so we can sort it by distance
 			for (i=0; i<response.results.length; i++) {
@@ -44,8 +74,21 @@ $(function () {
 			$body.html($list);
 
 			$.each(response.results, function (i, item) {
-				$body.append('<li><a href="#" class="point"><strong class="essid">' + item.essid + '</strong><br/><strong>Key:</strong> '+item.wpakey+' &mdash; ' + item.dist + ' meters<br/>'+item._id+'</a></li>');
+				$list.append('<li><a href="#" class="point"><strong class="essid">' + item.essid + '</strong><br/><strong>Key:</strong> '+item.wpakey+' &mdash; ' + item.dist + ' meters<br/>'+item._id+'</a></li>');
+
+				if (isMobileMode === false) {
+					var pos = new google.maps.LatLng(item.loc[0], item.loc[1]);
+					centerCoords.extend(pos);
+					new google.maps.Marker({
+						map: map,
+						position: pos
+					});
+				}
 			});
+
+			if (isMobileMode === false) {
+				map.fitBounds(centerCoords);
+			}
 		});
 	}
 
